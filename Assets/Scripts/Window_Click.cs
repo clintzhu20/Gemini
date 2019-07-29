413e9d23f5059f5387c9f886b1b2df6f051c2329
MrChen233 <qianxichen233@outlook.com>
2019-07-30 06:42:26 +0800

    wekjwq

---
+++ Assets/Scripts/Window_Click.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_Click : MonoBehaviour
+{
+    public GameObject[] cl;
+    public bool[] cover={false,false,false,false};
+    int LstPos=-1;
+    int GetMousePos(Vector3 pos)
+    {
+        if(pos.x>=212&&pos.x<=904&&pos.y<=1141&&pos.y>=660) return 0;
+        if(pos.x>=1247&&pos.x<=1937&&pos.y<=1141&&pos.y>=660) return 1;
+        if(pos.x>=212&&pos.x<=904&&pos.y<=536&&pos.y>=47) return 2;
+        if(pos.x>=1247&&pos.x<=1937&&pos.y<=536&&pos.y>=47) return 3;
+        return 4;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            int Pos=GetMousePos(Input.mousePosition);
+            print(Input.mousePosition.x);
+            print(Input.mousePosition.y);
+            if(Pos==4) return;
+            print(Pos);
+            if(cover[Pos]) return;
+            cover[Pos]=true;
+            GetComponent<Window_Random>().cover[Pos]=true;
+            if(GetComponent<Window_Random>().flag[Pos])
+            {
+                GetComponent<Window_Random>().count--;
+                GetComponent<Window_Random>().Win[Pos].GetComponent<SpriteRenderer>().enabled=false;
+            }
+            GetComponent<Window_Random>().flag[Pos]=false;
+            cl[Pos].GetComponent<SpriteRenderer>().enabled=true;
+            if(LstPos!=-1&&!cover[LstPos]) return;
+            if(LstPos!=-1)
+            {
+                cover[LstPos]=false;
+                GetComponent<Window_Random>().cover[LstPos]=false;
+                cl[LstPos].GetComponent<SpriteRenderer>().enabled=false;
+            }
+            LstPos=Pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Window_Random.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_Random : MonoBehaviour
+{
+    public GameObject[] Win;
+    public  bool[] flag={false,false,false,false};
+    public  bool[] cover={false,false,false,false};
+    float NowT,LstT;
+    bool Timeflag=false;
+    public int count=0;
+    void Start()
+    {
+        NowT=LstT=Time.fixedTime;
+    }
+    void Update()
+    {
+        //print(count);
+        if(count>=3) return;
+        NowT=Time.fixedTime;
+        if(NowT-LstT>=0.6f) Timeflag=false;
+        if(Timeflag) return;
+        int ShowWin;
+        //do
+        //{
+            ShowWin=Random.Range(0,4);
+        //if(cover[ShowWin]) ShowWin=(ShowWin==3?0:ShowWin+1);
+        int tmpcount=0;
+        while(flag[ShowWin]||cover[ShowWin])
+        {
+            ShowWin=(ShowWin==3?0:ShowWin+1);
+            tmpcount++;
+            if(tmpcount>=5) return;
+        }
+        //}while(flag[ShowWin]||cover[ShowWin]);
+        Win[ShowWin].GetComponent<SpriteRenderer>().enabled=true;
+        flag[ShowWin]=true;
+        count++;
+        Timeflag=true;
+        NowT=LstT=Time.fixedTime;
+    }
+}
